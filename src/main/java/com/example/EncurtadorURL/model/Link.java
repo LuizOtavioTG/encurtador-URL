b999d9c94f19f563ad7d7ed43bd18d21d1b5e704
Luiz <bc.luizo@gmail.com>
2025-02-27 21:07:05 -0300

    criando entidade Link

---
+++ src/main/java/com/example/EncurtadorURL/model/Link.java
@@ -0,0 +1,37 @@
+package com.example.EncurtadorURL.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "links")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Link {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String urlLong;
+    private String urlShort;
+    private String urlQrCode;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+
+    private LocalDateTime expiryAt;
+    private Integer views;
+}
